76232683cef293413f564bd86afe8be6a55ecc92
heunderhandstofirst <46429476+heunderhandstofirst@users.noreply.github.com>
2024-08-18 12:25:09 -0400

    trying urth

--- sketch.js
+++ sketch.js
@@ -290,7 +290,7 @@ function draw() {
 //////////////////////////////////////////////////////////////////////////
 // Which signTime = [hour(), minute(), second(), 60, 300];
 //  WhichSign=int(((Date.now() % 300000)/1000)/(300/29))
-// WhichSign=3
+WhichSign=14
 //////////////////////////////////////////////////////////////////////////
 frameRate(25);
 if (WhichSign===17) frameRate(10)
